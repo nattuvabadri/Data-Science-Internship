4e32aa707c04821a0a5d695988eadcfdb385f930
Badrinath Nattuva <124555982+nattuvabadri@users.noreply.github.com>
2023-03-25 17:50:57 +0530

    Add files via upload

---
+++ Proj-1/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Hello Everyone")
+st.header("I'm Badrinath Nattuva")
+st.subheader("Data Scientist Intern")
+st.caption("• Data Scientists develop machine learning models that improve the accuracy of predictions by X%.")
+st.caption("• Data cleaning and pre-processing, ensuring accuracy and completeness of data sets used for analysis.")
+st.caption("• Working with SQL Databases to extract and manipulate data for analysis, Improving query performance by optimizing the code.")
+st.caption("• Data Analysis using statistical methods to uncover tends and patterns in large data sets.")
+st.caption("• Create interactive dashboards using Tableau to Visualize data and report the insights.")
+st.markdown("""GitHub: ["GitHub Profile Link"](https://github.com/nattuvabadri).\n
+LinkdeIn: ["LinkdeIn Profile Link"](https://in.linkedin.com/in/badrinattuva)""")
