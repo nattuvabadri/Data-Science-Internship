00058a265327264c87d7941fc95ac664eb88aa97
Badrinath Nattuva <124555982+nattuvabadri@users.noreply.github.com>
2023-03-25 18:00:49 +0530

    Delete DashBoard directory

--- DashBoard/dashapp.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.header("I'm Badrinath Nattuva")
-st.subheader("Aspiring Data Scientist")
-st.caption("Here this Application hepls to find out different types DashBoards.")
